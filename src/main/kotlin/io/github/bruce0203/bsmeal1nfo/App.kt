10e5a8f373c7bad0c9943fa314d5bb7a39801578
KyoungMin Choi <ckm0728wash@gmail.com>
2024-01-29 19:29:31 +0900

    FIX: change substring index

--- src/main/kotlin/io/github/bruce0203/bsmeal1nfo/App.kt
+++ src/main/kotlin/io/github/bruce0203/bsmeal1nfo/App.kt
@@ -9,6 +9,7 @@ import java.lang.RuntimeException
 import java.text.SimpleDateFormat
 import javax.imageio.ImageIO
 import kotlin.system.exitProcess
+import kotlin.math.*
 
 fun main(args: Array<String>) {
     publish()
@@ -31,35 +32,36 @@ fun publish() {
             jpgFile.readBytes()
         })
         val options = MediaConfigureTimelineRequest.MediaConfigurePayload()
-            .caption(
-                """
+                .caption(
+                        """
                 `${it.title}` 새로운 글이 업로드됐어요!
+                
                 ${
-                    it.content.substring(
-                        0,
-                        200
-                    )
-                }
+                            it.content.substring(
+                                    0,
+                                    200.coerceAtMost(it.content.length)
+                            )
+                        }
                 ...
 
                 더 보려면 프로필의 블로그 링크에 접속해주세요!
                 이 글은 봇에 의해 자동 작성된 글입니다. - Developed by KimWash
             """.trimIndent()
-            )
+                )
 
         client.actions()
-            .timeline()
-            .uploadPhoto(image, options)
-            .thenAccept {
-                println(
-                    """
+                .timeline()
+                .uploadPhoto(image, options)
+                .thenAccept {
+                    println(
+                            """
                     --------------------------
                    "Successfully uploaded!"
                     --------------------------
                 """.trimIndent()
-                )
-            }
-            .join() // block current thread until complete
+                    )
+                }
+                .join() // block current thread until complete
     }
     exitProcess(0)
 }
